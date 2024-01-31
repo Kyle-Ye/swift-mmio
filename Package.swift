3c20ccc9f01341a66154b9ca1dbc20854162f4c8
Rauhul Varma <rauhul@apple.com>
2024-01-31 14:00:36 -0800

    Add SVD2LLDB LLDB plugin
    Introduce the SVD2LLDB plugin to improve debugging of MMIO registers
    using LLDB. It leverages the `SVD` library to parse CMSIS SVD files,
    enabling you to interact with device registers by name rather than raw
    memory addresses and values.
    
    Example uses of SVD2LLDB:
    
    1. Load the SVD file corresponding to the target device:
    
      ```console
      (lldb) svd load ~/Downloads/STM32F7x6.svd
      Loaded SVD file: “STM32F7x6.svd”
      ```
    
    2. Read the value of a hardware register using its name:
    
      ```console
      (lldb) svd read GPIOA.MODER
      GPIOA.MODER: 0x2800_0000
      ```
    
    3. Modify the value of a hardware register using its name:
    
      ```console
      (lldb) svd write GPIOA.MODER 0x12345678
      GPIOA.MODER: 0x12345678
      ```
    
    4. Decode the value `0x0123_4567` for register `TIMER0.CR` with a visual
       diagram:
    
      ```console
      (lldb) svd decode TIMER0.CR 0x0123_4567 --visual
      TIMER0.CR: 0x0123_4567
    
                            ╭╴CNTSRC  ╭╴RST
        ╭╴S   ╭╴RELOAD╭╴CAPEDGE  ╭╴MODE
        ┴     ┴─      ┴─    ┴─── ┴──  ┴
      0b00000001001000110100010101100111
            ┬─    ┬─    ┬───    ┬   ┬─ ┬
            ╰╴IDR ╰╴TRGEXT      ╰╴PSC  ╰╴EN
                        ╰╴CAPSRC    ╰╴CNT
    
      [31:31] S       0x0 (STOP)
      [27:26] IDR     0x0 (KEEP)
      [25:24] RELOAD  0x1 (RELOAD1)
      [21:20] TRGEXT  0x2 (DMA2)
      [17:16] CAPEDGE 0x3
      [15:12] CAPSRC  0x4 (GPIOA_3)
      [11:8]  CNTSRC  0x5 (CAP_SRC_div32)
      [7:7]   PSC     0x0 (Disabled)
      [6:4]   MODE    0x6
      [3:2]   CNT     0x1 (Count_DOWN)
      [1:1]   RST     0x1 (Reset_Timer)
      [0:0]   EN      0x1 (Enable)
      ```
    
    SVD2LLDB is hidden behind a feature flag as it requires LLDB.framework
    headers to build which are not always available, see documentation for
    details on how to build the plugin.
    
    Additionally this commit includes some improvements internal integer
    parsing and printing logic.

--- Package.swift
+++ Package.swift
@@ -29,8 +29,12 @@ var package = Package(
     .library(name: "SVD", targets: ["SVD"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.2"),
+    .package(
+      url: "https://github.com/apple/swift-argument-parser.git",
+      from: "1.4.0"),
+    .package(
+      url: "https://github.com/apple/swift-syntax.git",
+      from: "509.0.2"),
   ],
   targets: [
     // MMIO
@@ -139,9 +143,40 @@ var package = Package(
       ]),
   ])
 
+// Replace these with a native SPM feature flags if/when supported.
+let svd2lldb = "FEATURE_SVD2LLDB"
+if featureIsEnabled(named: svd2lldb, override: nil) {
+  package.targets += [
+    .target(name: "CLLDB"),
+    .target(
+      name: "SVD2LLDB",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        "CLLDB",
+        "SVD",
+      ],
+      swiftSettings: [.interoperabilityMode(.Cxx)],
+      linkerSettings: [.linkedFramework("LLDB")]),
+    .testTarget(
+      name: "SVD2LLDBTests",
+      dependencies: ["SVD2LLDB"],
+      swiftSettings: [.interoperabilityMode(.Cxx)]),
+  ]
+
+  package.products.append(
+    .library(name: "SVD2LLDB", type: .dynamic, targets: ["SVD2LLDB"]))
+}
+
+// Package API Extensions
 func featureIsEnabled(named featureName: String, override: Bool?) -> Bool {
   let key = "SWIFT_MMIO_\(featureName)"
-  let environment = ProcessInfo.processInfo.environment[key] != nil
+  let environment: Bool
+  switch Context.environment[key]?.lowercased() {
+  case "1", "true", "yes", "y":
+    environment = true
+  default:
+    environment = false
+  }
   return override ?? environment
 }
 
